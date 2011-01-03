1e4edba16ab38490b3bda00e234fa952099688ac
assaf.mashiah <assaf.mashiah@29617723-9222-c5f1-8b5c-aa82f2322a32>
2011-01-03 19:57:33 +0000

    The friend list data structure

---
+++ NetworksLab2/src/FriendList.java
@@ -0,0 +1,12 @@
+
+public class FriendList {
+	
+	// The Friends nickmane
+	public String NickName;
+	
+	// IP of the friend
+	public String IP;
+	
+	// Port Number the friend is listening to
+	public int Port;
+}
