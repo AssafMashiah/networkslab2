ec86b087d7952d96cd452d23e1818d6a3a59bab2
assaf.mashiah <assaf.mashiah@29617723-9222-c5f1-8b5c-aa82f2322a32>
2011-01-04 15:58:42 +0000

    new 'equal' override

--- NetworksLab2/src/FriendInfo.java
+++ NetworksLab2/src/FriendInfo.java
@@ -19,6 +19,24 @@ public class FriendInfo {
 		this.Port = Integer.parseInt(info[2]);
 	}
 	
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if(this == obj)
+		{
+			return true;
+		}
+		
+		if(!(obj instanceof FriendInfo))
+		{
+			return false;
+		}
+		
+		FriendInfo other = (FriendInfo) obj;
+		
+		return (this.NickName == other.NickName && this.IP == other.IP && this.Port == other.Port);
+	}
+	
 	public FriendInfo(String nickname, String IP, int port)
 	{
 		this.NickName = nickname;
